91daf4b103323cf28afa4da2cfd0b35a446e04a9
Git-Sohail <sohailsiddique00001@gmail.com>
2026-06-05 21:34:45 +0545

    fix: set logo width w-40 to force visible size regardless of canvas padding

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -66,7 +66,7 @@ export default function Navbar() {
               <img
                 src="/Logo.png"
                 alt="Chandani Cosmetic"
-                className="h-24 w-auto object-contain transition-transform duration-300 group-hover:scale-105"
+                className="h-24 w-40 object-contain transition-transform duration-300 group-hover:scale-105"
               />
             </Link>
           </div>
